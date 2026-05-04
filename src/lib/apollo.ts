f4b5e482c66f775882a4f3f694fe34879bd74710
Veronicah Bironga <69029669+Ronnieb254@users.noreply.github.com>
2026-05-04 11:38:21 +0300

    yml

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql"
+  uri: "https://api.thoughtcanva.com/graphql"
 });
 
 const authLink = new ApolloLink((operation, forward) => {
